a234861f9097c940f0c93b06de0a2bd3f152f90f
lMarco-Dev <marco.dev2408@gmail.com>
2026-03-19 16:46:44 -0500

    First commit

---
+++ src/main/java/com/dosyemas/almacen/AlmacenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dosyemas.almacen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlmacenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlmacenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dosyemas/almacen/AlmacenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosyemas.almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmacenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
